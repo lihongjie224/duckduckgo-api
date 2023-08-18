c11239c249b373613b17916cf016da144b76508e
LiHongjie <lihongjie@codingwhip.cn>
2023-08-18 23:38:17 +0800

    update api

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from duckduckgo_search import DDGS
 app = Flask(__name__)
 
@@ -7,9 +7,10 @@ app = Flask(__name__)
 def search():  # put application's code here
     keywords = request.args.get('q')
     with DDGS() as ddgs:
-        results = ddgs.text(keywords, backend='lite')
-    print(results)
-    return results
+        results = ddgs.text(keywords, region='cn-zh', safesearch='off', timelimit='y')
+        results_list = [dict(item) for item in results]
+        return jsonify(results_list)
+
 
 
 if __name__ == '__main__':
